fix error with variable number of populations in time averaging of moments

sparse_weights/scripts/dmft.py
@@ -861,7 +861,7 @@ def run_first_stage_dmft(prms,rX,CVh,res_dir,rc,Twrm,Tsav,dt,which='both',return
     # r = full_r[:,-1]
     # Cr = full_Cr[:,-1,-1:-Nsav-1:-1]
     r = np.mean(full_r[:,-1:-Nsav-1:-1],-1)
-    Cr = np.zeros((2,Nsav))
+    Cr = np.zeros((len(r),Nsav))
     for i in range(Nsav):
         Cr[:,i] = np.mean(each_diag(full_Cr[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
     
@@ -944,7 +944,7 @@ def run_second_stage_dmft(first_res_dict,prms,rX,CVh,res_dir,rc,Twrm,Tsav,dt,ret
     # time average predicted moments
     dr = r[:2] - r[2:]
     # Cdr = full_Cdr[:,-1,-1:-Nsav-1:-1]
-    Cdr = np.zeros((2,Nsav))
+    Cdr = np.zeros((len(dr),Nsav))
     for i in range(Nsav):
         Cdr[:,i] = np.mean(each_diag(full_Cdr[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
 
@@ -1020,7 +1020,7 @@ def run_two_stage_dmft(prms,rX,CVh,res_dir,rc,Twrm,Tsav,dt,return_full=False):
     # r = full_r[:,-1]
     # Cr = full_Cr[:,-1,-1:-Nsav-1:-1]
     r = np.mean(full_r[:,-1:-Nsav-1:-1],-1)
-    Cr = np.zeros((2,Nsav))
+    Cr = np.zeros((len(r),Nsav))
     for i in range(Nsav):
         Cr[:,i] = np.mean(each_diag(full_Cr[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
     
@@ -1042,7 +1042,7 @@ def run_two_stage_dmft(prms,rX,CVh,res_dir,rc,Twrm,Tsav,dt,return_full=False):
     # time average predicted moments
     dr = r[:2] - r[2:]
     # Cdr = full_Cdr[:,-1,-1:-Nsav-1:-1]
-    Cdr = np.zeros((2,Nsav))
+    Cdr = np.zeros((len(dr),Nsav))
     for i in range(Nsav):
         Cdr[:,i] = np.mean(each_diag(full_Cdr[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
 
@@ -1156,9 +1156,9 @@ def run_first_stage_ring_dmft(prms,rX,cA,CVh,res_dir,rc,Twrm,Tsav,dt,sa=15,L=180
     rb = np.mean(full_rb[:,-1:-Nsav-1:-1],-1)
     ra = np.mean(full_ra[:,-1:-Nsav-1:-1],-1)
     rp = np.mean(full_rp[:,-1:-Nsav-1:-1],-1)
-    Crb = np.zeros((2,Nsav))
-    Cra = np.zeros((2,Nsav))
-    Crp = np.zeros((2,Nsav))
+    Crb = np.zeros((len(rb),Nsav))
+    Cra = np.zeros((len(ra),Nsav))
+    Crp = np.zeros((len(rp),Nsav))
     for i in range(Nsav):
         Crb[:,i] = np.mean(each_diag(full_Crb[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
         Cra[:,i] = np.mean(each_diag(full_Cra[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
@@ -1333,9 +1333,9 @@ def run_second_stage_ring_dmft(first_res_dict,prms,rX,cA,CVh,res_dir,rc,Twrm,Tsa
     # Cdrb = full_Cdrb[:,-1,-1:-Nsav-1:-1]
     # Cdra = full_Cdra[:,-1,-1:-Nsav-1:-1]
     # Cdrp = full_Cdrp[:,-1,-1:-Nsav-1:-1]
-    Cdrb = np.zeros((2,Nsav))
-    Cdra = np.zeros((2,Nsav))
-    Cdrp = np.zeros((2,Nsav))
+    Cdrb = np.zeros((len(drb),Nsav))
+    Cdra = np.zeros((len(dra),Nsav))
+    Cdrp = np.zeros((len(drp),Nsav))
     for i in range(Nsav):
         Cdrb[:,i] = np.mean(each_diag(full_Cdrb[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
         Cdra[:,i] = np.mean(each_diag(full_Cdra[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
@@ -1461,9 +1461,9 @@ def run_two_stage_ring_dmft(prms,rX,cA,CVh,res_dir,rc,Twrm,Tsav,dt,sa=15,L=180,r
     rb = np.mean(full_rb[:,-1:-Nsav-1:-1],-1)
     ra = np.mean(full_ra[:,-1:-Nsav-1:-1],-1)
     rp = np.mean(full_rp[:,-1:-Nsav-1:-1],-1)
-    Crb = np.zeros((2,Nsav))
-    Cra = np.zeros((2,Nsav))
-    Crp = np.zeros((2,Nsav))
+    Crb = np.zeros((len(rb),Nsav))
+    Cra = np.zeros((len(ra),Nsav))
+    Crp = np.zeros((len(rp),Nsav))
     for i in range(Nsav):
         Crb[:,i] = np.mean(each_diag(full_Crb[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
         Cra[:,i] = np.mean(each_diag(full_Cra[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
@@ -1511,9 +1511,9 @@ def run_two_stage_ring_dmft(prms,rX,cA,CVh,res_dir,rc,Twrm,Tsav,dt,sa=15,L=180,r
     # Cdrb = full_Cdrb[:,-1,-1:-Nsav-1:-1]
     # Cdra = full_Cdra[:,-1,-1:-Nsav-1:-1]
     # Cdrp = full_Cdrp[:,-1,-1:-Nsav-1:-1]
-    Cdrb = np.zeros((2,Nsav))
-    Cdra = np.zeros((2,Nsav))
-    Cdrp = np.zeros((2,Nsav))
+    Cdrb = np.zeros((len(drb),Nsav))
+    Cdra = np.zeros((len(dra),Nsav))
+    Cdrp = np.zeros((len(drp),Nsav))
     for i in range(Nsav):
         Cdrb[:,i] = np.mean(each_diag(full_Cdrb[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)
         Cdra[:,i] = np.mean(each_diag(full_Cdra[:,-1:-Nsav-i-1:-1,-1:-Nsav-i-1:-1],i),-1)